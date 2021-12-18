9ca749e989bb229d65066571ca60bbb9c53effa8
Koen Van Rulo <koen.vr@pm.me>
2021-12-18 19:27:02 +0100

    add the contract module to compile and deploy

---
+++ contract/build.go
@@ -0,0 +1,77 @@
+package contract
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/algorand/go-algorand-sdk/logic"
+
+	cfg "github.com/vecno-io/go-pyteal/config"
+	net "github.com/vecno-io/go-pyteal/network"
+)
+
+func Build(list []string) error {
+	fmt.Println(":: Contracts build:", cfg.DataPath())
+
+	for _, s := range list {
+		if err := build(s); nil != err {
+			return err
+		}
+		if err := compile(s); nil != err {
+			return err
+		}
+	}
+	return nil
+}
+
+func build(name string) error {
+	path := fmt.Sprintf("%s/contracts/%s", cfg.AssetPath(), name)
+	cmd := fmt.Sprintf("python3 %s.py > %s.teal", path, path)
+	fmt.Println(">>", cmd)
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if len(out) > 0 {
+		fmt.Println(string(out))
+	}
+	if nil != err {
+		return err
+	}
+	return nil
+}
+
+func compile(name string) error {
+	path := fmt.Sprintf("%s/contracts/%s", cfg.AssetPath(), name)
+	fmt.Println(">> compile", path)
+
+	cln, err := net.MakeClient()
+	if err != nil {
+		return fmt.Errorf("compile %s failed: make client: %s", name, err)
+	}
+
+	teal, err := ioutil.ReadFile(fmt.Sprintf("%s.teal", path))
+	if err != nil {
+		return fmt.Errorf("compile %s failed: read file: %s", name, err)
+	}
+
+	chk, err := cln.TealCompile(teal).Do(context.Background())
+	if err != nil {
+		return fmt.Errorf("compile %s failed: compile teal: %s", name, err)
+	}
+	prg, err := base64.StdEncoding.DecodeString(chk.Result)
+	if err != nil {
+		return fmt.Errorf("compile %s failed: decode program: %s", name, err)
+	}
+	err = logic.CheckProgram(prg, make([][]byte, 0))
+	if nil != err {
+		return fmt.Errorf("compile %s failed: check program: %s", name, err)
+	}
+
+	if err = ioutil.WriteFile(fmt.Sprintf("%s.prog", path), prg, os.ModePerm); nil != err {
+		return fmt.Errorf("compile %s failed: write file: %s", name, err)
+	}
+
+	return nil
+}
---
+++ contract/deploy.go
@@ -0,0 +1,132 @@
+package contract
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	cfg "github.com/vecno-io/go-pyteal/config"
+	net "github.com/vecno-io/go-pyteal/network"
+
+	"github.com/algorand/go-algorand-sdk/crypto"
+	"github.com/algorand/go-algorand-sdk/future"
+	"github.com/algorand/go-algorand-sdk/types"
+)
+
+type Setup struct {
+	Manager crypto.Account
+
+	ClearProg    string
+	ApprovalProg string
+
+	LocalSchema  types.StateSchema
+	GlobalSchema types.StateSchema
+}
+
+func GetId(name string) (uint64, error) {
+	return loadFromJsonFile(name)
+}
+
+func Deploy(s Setup) error {
+	optIn := true
+
+	clearProg, err := ioutil.ReadFile(fmt.Sprintf(
+		"%s/contracts/%s.prog", cfg.AssetPath(), s.ClearProg,
+	))
+	if err != nil {
+		return fmt.Errorf("deploy failed: %s: read file: %s", s.ClearProg, err)
+	}
+	approvalProg, err := ioutil.ReadFile(fmt.Sprintf(
+		"%s/contracts/%s.prog", cfg.AssetPath(), s.ApprovalProg,
+	))
+	if err != nil {
+		return fmt.Errorf("deploy failed: %s: read file: %s", s.ApprovalProg, err)
+	}
+
+	appArgs := [][]byte{}
+	accounts := []string{}
+	foreignApps := []uint64{}
+	foreignAssets := []uint64{}
+
+	cln, err := net.MakeClient()
+	if err != nil {
+		return fmt.Errorf("deploy failed: make client: %s", err)
+	}
+	txnParams, err := cln.SuggestedParams().Do(context.Background())
+	if err != nil {
+		return fmt.Errorf("deploy failed: suggested params: %s", err)
+	}
+
+	note := []byte{}
+	group := types.Digest{}
+	lease := [32]byte{}
+	rekeyTo := types.ZeroAddress
+	extraPages := uint32(0)
+
+	createTx, err := future.MakeApplicationCreateTxWithExtraPages(
+		optIn, approvalProg, clearProg, s.GlobalSchema, s.LocalSchema,
+		appArgs, accounts, foreignApps, foreignAssets, txnParams,
+		s.Manager.Address, note, group, lease, rekeyTo, extraPages,
+	)
+	if err != nil {
+		return fmt.Errorf("deploy failed: make create tx: %s", err)
+	}
+
+	// Enforce it or fail, a bug?
+	createTx.OnCompletion = types.OptInOC
+
+	_, signedTx, err := crypto.SignTransaction(s.Manager.PrivateKey, createTx)
+	if err != nil {
+		return fmt.Errorf("deploy failed: sign create tx: %s", err)
+	}
+	pendingTx, err := cln.SendRawTransaction(signedTx).Do(context.Background())
+	if err != nil {
+		return fmt.Errorf("deploy failed: send create tx: %s", err)
+	}
+
+	txConfirm, err := net.WaitForConfirmation(cln, pendingTx, 24, context.Background())
+	if err != nil {
+		return fmt.Errorf("deploy failed: confirm tx: %s", err)
+	}
+	if len(txConfirm.PoolError) > 0 {
+		return fmt.Errorf("deploy failed: pool error: %s", txConfirm.PoolError)
+	}
+
+	if err := saveToFile(s.ApprovalProg, txConfirm.ApplicationIndex); err != nil {
+		return fmt.Errorf("contract: failed to save app: %s", err)
+	}
+
+	return nil
+}
+
+func saveToFile(name string, id uint64) error {
+	str, err := json.Marshal(id)
+	if err != nil {
+		return err
+	}
+	// TODO Fix Path
+	if err = ioutil.WriteFile(fmt.Sprintf(
+		"%s/%s.id", cfg.AssetPath(), name,
+	), str, os.ModePerm); err != nil {
+		return err
+	}
+	return nil
+}
+
+func loadFromJsonFile(name string) (uint64, error) {
+	id := uint64(0)
+	// TODO Fix Path
+	data, err := os.ReadFile(fmt.Sprintf(
+		"%s/%s.id", cfg.AssetPath(), name,
+	))
+	if err != nil {
+		return 0, err
+	}
+	err = json.Unmarshal(data[:], &id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
